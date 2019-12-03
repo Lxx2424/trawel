4497750f5369c387caaff551ae9464edf63053c6
nihademin <nihad_emin@mail.ru>
2019-12-03 19:53:27 +0400

    Khazar3Dec

--- js/sliderIndex.js
+++ js/sliderIndex.js
@@ -2,6 +2,7 @@ $('.gallery-all').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
     speed: 1000,
+    autoplay: true,
     autoplaySpeed: 800,
     responsive: [
         {
@@ -9,6 +10,9 @@ $('.gallery-all').slick({
           settings: {
             slidesToShow: 4,
             slidesToScroll: 1,
+            autoplay: true,
+    autoplaySpeed: 800,
+
             infinite: true,
           }
         },
@@ -16,6 +20,9 @@ $('.gallery-all').slick({
           breakpoint: 768,
           settings: {
             slidesToShow: 3,
+            autoplay: true,
+    autoplaySpeed: 800,
+
             slidesToScroll: 1,
           }
         },
@@ -23,6 +30,9 @@ $('.gallery-all').slick({
           breakpoint: 576,
           settings: {
             slidesToShow: 1,
+                        autoplay: true,
+    autoplaySpeed: 800,
+
             slidesToScroll: 1,
           }
         }
@@ -30,4 +40,43 @@ $('.gallery-all').slick({
   });
 
 
-  
+  $('.slide-all').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 800,
+    responsive: [
+        {
+          breakpoint: 1140,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+    autoplaySpeed: 800,
+
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            autoplay: true,
+    autoplaySpeed: 800,
+
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+                        autoplay: true,
+    autoplaySpeed: 800,
+
+            slidesToScroll: 1,
+          }
+        }
+      ]
+  });
